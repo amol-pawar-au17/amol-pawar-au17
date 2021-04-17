48c9fbd80ec4687b3a6d0631aec2aacf5ef62357
amol-pawar-au17 <82273409+amol-pawar-au17@users.noreply.github.com>
2021-04-18 00:28:15 +0530

    Add files via upload

---
+++ weekcc2.py
@@ -0,0 +1,33 @@
+a. Write a program to take in 2 numbers as input and print their +, -, /, 
+//, % and *
+b. Write a program that takes in 2 numbers and returns true if they are 
+divisible by each other. False otherwise.
+[hint: use the bool() function]
+
+a.
+
+a=5
+b=7
+c=a+b
+input(c)
+
+d=a-b
+input(d)
+
+e=a/b
+input(e)
+
+h=b//a
+print(h)
+
+g=b%a
+print(g)
+
+i=a*b
+print(i)
+
+b.
+num1=int("please enter 1st number")
+num2=int("please enter2nd number")
+
+print=("num 1% num2=0 or num2%num1=0")
