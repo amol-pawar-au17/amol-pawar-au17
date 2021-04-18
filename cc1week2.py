fec8aaa8cd4cbdcd0452dc8c2781670110cd4aa3
amol-pawar-au17 <82273409+amol-pawar-au17@users.noreply.github.com>
2021-04-18 13:49:14 +0530

    Add files via upload

---
+++ cc1week2.py
@@ -0,0 +1,15 @@
+#Python Intro
+#1. Write python program to ask a user's name, age and telephone number 
+#and welcome the user.
+#2. Given 2 numbers as input from the user, find their sum and print it.
+
+#1.
+name=input("what is your name:")
+age=input("what is your age:")
+mobilenumber=input("what is your mobile number:")
+
+#2.
+num1=input("please give a 1st number:")
+num2=input("please give a 2nd number:")
+pr=num1+num2
+print(pr)
