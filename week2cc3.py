bce626195e0bc58b45de5526ce4b92d2ecd3a953
amol-pawar-au17 <82273409+amol-pawar-au17@users.noreply.github.com>
2021-04-18 12:24:20 +0530

    Add files via upload

---
+++ week2cc3.py
@@ -0,0 +1,11 @@
+#a. Take 2 numbers as input from the user and print True if the first 
+#number is greater, false otherwise.
+#b. Take 1 number as input from the user. Print its table from 1 to 10.
+#a.
+num1 =input("please enter first number:")
+num2=input("please enter second number:")
+print(bool(num1>num2))
+#b.
+pr=input("please enter one any number:")
+print(pr*1*10)
+
